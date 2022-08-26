463bae3db7a39b82b8785955bfb81f5e0ebc7bfa
TherionRO <59541950+TherionRO@users.noreply.github.com>
2022-08-27 01:20:41 +0300

    Add files via upload

---
+++ Mods List/OptimizationPack_By_TherionRO.java
@@ -0,0 +1,3 @@
+import finally a good minecraft experience.
+
+Optimization Pack is a open-source and lightweight modification for the official Minecraft,supporting all versions.With OP,you will get way more FPS,even 10X sometimes,and there are a ton of minecraft bugs fixed in this modification.You won't get banned on any server for using this,performance boost will also work on servers,not only singleplayer-side.
